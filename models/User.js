46f8a5a1ff1474a29630ee9aa6edb60a48bc2042
lucas <lereveur731@gmail.com>
2024-10-05 07:17:15 +0200

    paimement

--- models/User.js
+++ models/User.js
@@ -5,8 +5,12 @@ const jwt = require('jsonwebtoken');
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  credits: { type: Number, default: 0 },
-  stripeCustomerId: { type: String }
+  tokens: { type: Number, default: 0 },
+  stripeCustomerId: { type: String },
+  stripeSubscriptionId: { type: String },
+  stripePlanId: { type: String },
+  subscriptionStatus: { type: String, enum: ['active', 'canceled', 'none'], default: 'none' },
+  subscriptionEndDate: { type: Date }
 });
 
 userSchema.pre('save', async function(next) {
@@ -24,18 +28,45 @@ userSchema.methods.generateAuthToken = function() {
   return jwt.sign({ _id: this._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
 };
 
-userSchema.methods.addCredits = async function(amount) {
-  this.credits += amount;
+userSchema.methods.addTokens = async function(amount) {
+  this.tokens += amount;
   await this.save();
 };
 
-userSchema.statics.findByEmail = function(email) {
-  return this.findOne({ email });
+userSchema.methods.resetTokens = async function(amount) {
+  this.tokens = amount;
+  await this.save();
+};
+
+userSchema.methods.useToken = async function() {
+  if (this.tokens > 0) {
+    this.tokens -= 1;
+    await this.save();
+    return true;
+  }
+  return false;
 };
 
 userSchema.methods.setStripeCustomerId = async function(customerId) {
   this.stripeCustomerId = customerId;
   await this.save();
+};
+
+userSchema.methods.setSubscription = async function(subscriptionId, planId, endDate) {
+  this.stripeSubscriptionId = subscriptionId;
+  this.stripePlanId = planId;
+  this.subscriptionStatus = 'active';
+  this.subscriptionEndDate = endDate;
+  await this.save();
+};
+
+userSchema.methods.cancelSubscription = async function() {
+  this.subscriptionStatus = 'canceled';
+  await this.save();
+};
+
+userSchema.statics.findByEmail = function(email) {
+  return this.findOne({ email });
 };
 
 const User = mongoose.model('User', userSchema);
